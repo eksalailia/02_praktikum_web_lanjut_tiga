1d71fb7b9d61d075aa30ecf26d1d74a38ddd78fe
eksalailia <eksalailia12@gmail.com>
2021-03-05 01:06:10 +0700

    first commit

---
+++ app/Http/Controllers/HomeCVController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class HomeCVController extends Controller
+{
+    public function index(){
+        return 'Halaman dari awal website'; 
+    }
+  
+}
